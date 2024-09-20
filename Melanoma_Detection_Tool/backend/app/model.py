96bd914bc28c1ef8f2d12eb11f8d6d57b20d199c
nacjacds <168210578+nacjacds@users.noreply.github.com>
2024-09-20 13:50:56 +0200

    update

--- Melanoma_Detection_Tool/backend/app/model.py
+++ Melanoma_Detection_Tool/backend/app/model.py
@@ -1,29 +1,36 @@
-from tensorflow.keras.models import load_model
 import os
+from tensorflow.keras.models import load_model
+import numpy as np
 
-# Ruta del directorio del modelo
-model_dir = "Melanoma_Detection_Tool/backend/model/"
+# Ruta relativa al archivo .keras
+model_dir = os.path.dirname(__file__)  # Obtiene el directorio actual del archivo .py
+model_path = os.path.join(model_dir, '..', 'model', 'melanoma-diagnosis.keras')  # Apunta al archivo dentro de la carpeta 'model'
 
-# Imprimir el contenido del directorio para verificar si el archivo está presente
-print("Archivos en la carpeta 'model':", os.listdir(model_dir))
-
-# Ruta al archivo de modelo
-model_path = os.path.join(model_dir, "melanoma-diagnosis.h5")
-
-# Verificar si el archivo de modelo existe
+# Verifica si el archivo existe y carga el modelo
 if os.path.exists(model_path):
-    print(f"Archivo de modelo encontrado en {model_path}. Cargando el modelo...")
     model = load_model(model_path)
+    print(f"Modelo cargado correctamente desde {model_path}")
 else:
     raise FileNotFoundError(f"No se encontró el archivo de modelo en {model_path}")
 
-# Función de predicción
-def predict_melanoma(image):
-    # Aquí va la lógica para preprocesar la imagen y realizar predicciones
-    return model.predict(image)
+def predict_melanoma(processed_image):
+    """ Realiza la predicción usando el modelo cargado """
+    # La imagen debe tener la forma (224, 224, 3)
+    image_batch = np.expand_dims(processed_image, axis=0)  # Expande las dimensiones de la imagen
+    prediction = model.predict(image_batch)[0]  # Realiza la predicción y toma el primer valor del lote
     
-    # Calcula el porcentaje de certeza basado en la predicción
+    # Procesar la predicción
+    melanoma_probability = prediction[0]  # Supongamos que el modelo tiene una sola salida
+    
+    # Definir el umbral para el diagnóstico
+    threshold = 0.5
+    if melanoma_probability >= threshold:
+        diagnosis = "Positivo para melanoma"
+    else:
+        diagnosis = "Negativo para melanoma"
+    
+    # Calcular la certeza
     certainty = melanoma_probability * 100 if melanoma_probability >= threshold else (1 - melanoma_probability) * 100
 
-    # Retorna el diagnóstico y el porcentaje de certeza
+    # Retornar el diagnóstico y la certeza
     return diagnosis, certainty
