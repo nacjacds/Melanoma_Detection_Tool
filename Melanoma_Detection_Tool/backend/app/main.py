0b793d507452c7b4185420c9b03affc36373c90e
nacjacds <168210578+nacjacds@users.noreply.github.com>
2024-09-20 23:36:22 +0200

    Update main.py

--- Melanoma_Detection_Tool/backend/app/main.py
+++ Melanoma_Detection_Tool/backend/app/main.py
@@ -7,11 +7,7 @@ from PIL import Image
 st.title("Melanoma Detection Tool")
 
 # Subir imagen usando Streamlit
-uploaded_file = st.file_uploader("""This system uses artificial intelligence to help detect potential melanomas. The model has been trained to differentiate between melanomas and other skin lesions. However, it is important to note that some lesions, which may also be cancerous, could be classified as 'Non-Melanoma'.
-
-Melanoma is the most aggressive type of skin cancer, and early detection is crucial to increase the chances of successful treatment. There are other lesions, such as Lentigo Maligna, which, although not melanomas in their initial stage, have the potential to develop into melanomas if left untreated.
-
-This system does not replace a professional medical evaluation. Any suspicious lesion should be examined by a dermatologist for proper diagnosis and treatment. This tool in no way replaces a professional medical assessment.""", type=["jpg", "jpeg", "png"])
+uploaded_file = st.file_uploader("""Upload the clearest possible image of a mole or skin lesion to receive a melanoma risk diagnosis. please be aware that the algorithm's effectiveness may be influenced by factors such as the image's characteristics, angle, lighting, contrast, and overall quality.""", type=["jpg", "jpeg", "png"])
 
 
 
@@ -35,5 +31,16 @@ if uploaded_file is not None:
 
     # Añadir un texto informativo debajo del área de carga de imagen
 st.write("""
-Please ensure that the image is clear and well-lit. The quality of the image can significantly impact the accuracy of the diagnosis.
+This system uses artificial intelligence to help detect potential melanomas. The model has been trained on thousands of images of benign moles, melanomas, and other types of skin lesions. It is a binary classification model, meaning it will only provide a diagnosis of Melanoma or NotMelanoma.
+The focus on melanoma is due to it being the most aggressive and dangerous form of skin cancer. However, the NotMelanoma category may include benign moles, seborrheic keratosis, and less aggressive forms of skin cancer that may still require medical attention. 
+
+There are also other lesions, such as Lentigo Maligna, which, while not melanomas in their initial stage, have the potential to develop into melanomas if left untreated.
+
+Melanoma is the most aggressive type of skin cancer, and early detection is crucial to improve the chances of successful treatment.
+Although the model has been trained to differentiate between melanomas and other skin lesions, it is important to note that some cancerous lesions could be classified as NotMelanoma. Therefore, this system should not be considered a substitute for a professional medical evaluation. Any suspicious lesion should be examined by a dermatologist for proper diagnosis and treatment.
+
+My aim is to make the model as accurate as possible, but please be aware that the algorithm's effectiveness may be influenced by factors such as the image's characteristics, angle, lighting, contrast, and overall quality.
+
+Disclaimer: This is an anonymous and free application, and it is not intended to replace a doctor's diagnosis. If you have any concerns about a mole or skin lesion, you should consult a healthcare professional.
+This project is part of my training as a Data Scientist, aimed at demonstrating the skills I have developed and showcasing them in my portfolio.
 """)
