71268bac2cfeea4fa374b5005c8b008a38f5163d
nacjacds <168210578+nacjacds@users.noreply.github.com>
2024-09-20 14:40:05 +0200

    Update main.py

--- Melanoma_Detection_Tool/backend/app/main.py
+++ Melanoma_Detection_Tool/backend/app/main.py
@@ -1,28 +1,19 @@
-import streamlit as st
-from backend.app.utils import save_image, preprocess_image
-from backend.app.model import predict_melanoma
-from PIL import Image
+from utils import save_image, preprocess_image
+from model import predict_melanoma
+from fastapi import FastAPI, File, UploadFile
 
-# Título de la aplicación
-st.title("Melanoma Detection Tool")
+app = FastAPI()
 
-# Subir la imagen
-uploaded_file = st.file_uploader("Sube una imagen para predecir si tiene melanoma", type=["jpg", "png", "jpeg"])
+@app.post("/predict/")
+async def predict(file: UploadFile = File(...)):
+    # Guarda la imagen subida
+    image_path = save_image(file)
 
-if uploaded_file is not None:
-    # Mostrar la imagen subida
-    image = Image.open(uploaded_file)
-    st.image(image, caption="Imagen subida", use_column_width=True)
-
-    # Guardar la imagen en el directorio 'uploads'
-    image_path = save_image(uploaded_file)
-
-    # Preprocesar la imagen
+    # Preprocesa la imagen para el modelo
     processed_image = preprocess_image(image_path)
 
-    # Realizar la predicción
-    diagnosis, certainty = predict_melanoma(processed_image)
+    # Realiza la predicción con el modelo
+    result = predict_melanoma(processed_image)
 
-    # Mostrar los resultados de la predicción
-    st.write(f"Diagnóstico: {diagnosis}")
-    st.write(f"Porcentaje de certeza: {certainty:.2f}%")
+    # Devuelve el resultado de la predicción
+    return {"diagnosis": result}
