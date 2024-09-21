9e11d9fd04df265ee008cc5708163a5ee0024163
nacjacds <168210578+nacjacds@users.noreply.github.com>
2024-09-21 11:14:32 +0200

    Update main.py

--- Melanoma_Detection_Tool/backend/app/main.py
+++ Melanoma_Detection_Tool/backend/app/main.py
@@ -2,15 +2,19 @@ import streamlit as st
 from utils import save_image, preprocess_image
 from model import predict_melanoma
 from PIL import Image
+import os  # Importar el módulo os
 
-# Mostrar la imagen
-# Ruta a la imagen (relativa desde main.py)
-image_path = '../../assets/lunares.jpg'
-try:
+# Generar la ruta absoluta de la imagen
+image_path = os.path.abspath('assets/lunares.jpg')
+
+# Verificar si la imagen está en la ruta correcta
+st.write(f"Ruta absoluta: {image_path}")  # Mostrar la ruta para verificar
+
+if os.path.exists(image_path):
     image = Image.open(image_path)
     st.image(image, caption='Detección de Melanoma', use_column_width=True)
-except FileNotFoundError:
-    st.error("La imagen no se pudo cargar. Verifica la ruta del archivo.")
+else:
+    st.error(f"La imagen no se encontró en la ruta absoluta: {image_path}")
 
 # Título de la aplicación
 st.title("Melanoma Detection Tool")
